5f135e7fc4b9e1b1d35d59768e28ba047c650c95
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-07-14 08:56:49 +0200

    Favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        {{-- FAVICON --}}
+        <link rel="shortcut icon" href="{{ asset("img/favicon.ico") }}" type="image/x-icon">
         {{-- STYLE CSS --}}
         <link rel="stylesheet" href="{{ asset("css/app.css") }}">
         {{-- TITLE --}}
